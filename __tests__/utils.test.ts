d9f7b5e088b854c30d83abf3367dbb087f2b6ed6
Boroukanlou <s.boroukanlou@gmail.com>
2025-10-31 11:08:15 +0330

    first commit, project ready for operation

--- __tests__/utils.test.ts
+++
@@ -1,8 +0,0 @@
-import { generateToken } from '../src/lib/utils'
-
-test('generateToken returns a non-empty string', () => {
-  const t = generateToken()
-  expect(typeof t).toBe('string')
-  expect(t.length).toBeGreaterThan(5)
-})
-
